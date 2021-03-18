0aabf40ced4d84395fd4ff2bcfa9d78a6a434bd5
dlwndms0812 <dlwndms0812@naver.com>
2021-03-19 02:17:42 +0900

    p.49 HelloMFC.cpp

--- HelloMFC/HelloMFC.cpp
+++ HelloMFC/HelloMFC.cpp
@@ -1,10 +1,10 @@
-#include <afxwin.h>
+#include <afxwin.h>  //헤더파일
 
-//응용 프로그램 클래스를 선언한다.
+//응용 프로그램 클래스를 선언한다.   //클래스 선언부
 class CHelloApp :public CWinApp
 {
 public:
-	virtual BOOL InitIntance();
+	virtual BOOL InitInstance();
 };
 
 //메인 윈도우 클래스를 선언한다.
@@ -15,14 +15,14 @@ public:
 
 protected:
 	afx_msg void OnPaint();
-	afx_msg void OnLButtonDown(UINt nFlags, CPoint point);
-	DECLEARE_MESSAGE_MAP()
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	DECLARE_MESSAGE_MAP()
 };
 
-//응용 프로그램 객체를 선언한다.
+//응용 프로그램 객체를 선언한다.    //응용 프로그램 객체
 CHelloApp theApp;
 
-//응용 프로그램 클래스를 정의한다.
+//응용 프로그램 클래스를 정의한다.  //클래스 정의부 
 BOOL CHelloApp::InitInstance()
 {
 	m_pMainWnd = new CMainFrame;
@@ -31,6 +31,25 @@ BOOL CHelloApp::InitInstance()
 }
 
 //메인 윈도우 클래스를 정의한다.
-CMainFrame::CMainFrame() {
-
+CMainFrame::CMainFrame() 
+{
+	Create(NULL, _T("HelloMFC"));
 }
+
+void CMainFrame::OnPaint()
+{
+	CPaintDC dc(this);
+	const TCHAR* msg = _T("Hello, MFC");
+	dc.TextOut(100, 100, msg, lstrlen(msg));
+}
+
+void CMainFrame::OnLButtonDown(UINT nFlag, CPoint point)
+{
+	MessageBox(_T("마우스 클릭!"), _T("마우스 메시지"));
+}
+
+//메시지 맵을 선언한다.          //메시지 맵
+BEGIN_MESSAGE_MAP(CMainFrame,CFrameWnd)
+	ON_WM_PAINT()
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
